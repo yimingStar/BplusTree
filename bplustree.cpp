15f361874088366695f442366ef0cf82f7068f0a
YiMing <ericdreamwithvic@gmail.com>
2021-03-27 00:31:25 -0400

    [feature] build treeNode class
    1. [TODO] figure out how to cleanly store child pointer

--- bplustree.cpp
+++ bplustree.cpp
@@ -1,5 +1,82 @@
 #include "bplustree.h"
-bPlusTree::bPlusTree(int n): degree(n) {} 
+#include <iostream>
+using namespace std;
+
+/**
+ * Build treeNode class
+ * ====================
+*/
+treeNode::treeNode(int key, double value) {
+  // add new key-value pairs into leaf node
+  keyPairs.insert({key, value});
+}
+
+
+/**
+ * Build bPlusTree class
+ * =====================
+*/
+/**
+ * @brief Construct a new b Plus Tree::b Plus Tree object
+ * 
+ * @param m 
+ */
+bPlusTree::bPlusTree(int m) {
+  degree = m;
+  root = NULL;
+}
+
+/**
+ * @brief 
+ * 
+ * @return int = tree degree
+ */
 int bPlusTree::getTreeDegree() {
  return degree;
+}
+
+/**
+ * @brief 
+ * @return int - {0} = success, {-1} = failed
+ */
+int bPlusTree::insertion(int key, double value) {
+  /**
+   * No need to search the tree first (the insert key value has no duplication)
+  */
+  try {
+    treeNode *targetNode = root;
+    if(targetNode == NULL) {
+      targetNode = new treeNode(key, value);
+      return 0;
+    }
+
+    while(targetNode != NULL) {
+      
+    }
+  }
+  catch(exception& e) {
+    cerr << "exception caught: " << e.what() << '\n';
+  }
+  return 0;
+}
+
+/**
+ * @brief 
+ * 
+ * @param key 
+ * @return int 
+ */
+int bPlusTree::deletion(int key) {
+  return 0;
+}
+
+/**
+ * @brief 
+ * 
+ * @param arr 
+ * @return int - {0} = success, {-1} = failed
+ */
+int bPlusTree::search(int arr[]) {
+  // insert into ouput.txt
+  return 0;
 }
--- bplustree.h
+++ bplustree.h
@@ -1,26 +1,48 @@
-// <PROJECT>_<PATH>_<FILE>_H_.
+#include<list>
+#include<vector>
+#include<map>
+#include<stack>
 
+// <PROJECT>_<PATH>_<FILE>_H_.
 #ifndef BPLUSTREE_H_
 #define BPLUSTREE_H_
+
+
+class treeNode {
+  private:
+    /**
+     * Size of this map will be degree-1
+     * Which will be the maximum key pairs in one node. 
+    */
+    bool isLeaf;  
+    std::map<int,double> keyPairs; 
+  public:
+    treeNode(int key); // create index node
+    treeNode(int key, double value); // create leaf node
+    int getNodeNumOfPairs();
+    int getIsLeaf();
+};
 
 class bPlusTree {
   private: 
     int degree;
+    treeNode *root;
+    std::stack<treeNode> tracepath;
+    std::list<treeNode> leafs;
   public:
-  
   /**
    * project description functions
    */
-  bPlusTree(int n);
-  void insertion();
-  void deletion();
-  void search(int arr[]);
-
+  bPlusTree(int m);
+  int insertion(int key, double value);
+  int deletion(int key);
+  int search(int arr[]);
 
   /**
    * test functions
    */
   int getTreeDegree();
+  void traversal(); 
 };
 
 #endif  // BPLUSTREE_H_
